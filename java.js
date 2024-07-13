e2611fc1b9db645ec409fb6b818f3dc197bf7c54
rbrkty <rbrkty@protonmail.com>
2024-07-13 17:34:13 +0200

    update

--- java.js
+++ java.js
@@ -1,61 +1,103 @@
+
 const btnCnt = document.getElementById("share-button");
-btnCnt.addEventListener("click", ToggleActive, false);
+btnCnt.addEventListener("click", SelectToggleFunction, false);
 
-const userinfoshare = document.getElementById("user-info-share");
-const userinfoshareactive = document.getElementById("user-info-share-active");
-const socialmediaicons = document.getElementById("social-media-icons");
+window.addEventListener("resize", OnResize);
 
+const avatarcontainer = document.getElementById("avatar-container");
 const avatarimage = document.getElementById("avatar");
 const p_username = document.getElementById("username");
 const p_date = document.getElementById("date");
-const p_sharemsg = document.getElementById("share-msg");
+const div_userinfo = document.getElementById("user-info");
 
+const sharebutton = document.getElementById("share-button");
 const svg_sharebutton = document.getElementById("share-button-svg");
 
-const socialmediapopup = document.getElementById("social-media-icons-popup");
+const share_msg = document.getElementById("share-msg");
+const socialmediaicons = document.getElementById("social-media-icons");
 
-function ToggleActive() {
-    if(window.innerWidth < 870) {
-        // set share button inactive
-        if(userinfoshare.classList.contains("user-info-share-active")) {
-            userinfoshare.classList.remove("user-info-share-active");
+const socialmediaiconspopup = document.getElementById("social-media-icons-popup");
 
-            avatarimage.style.display = "block";
-            p_username.style.display = "block";
-            p_date.style.display = "block";
+function OnResize() {
+    socialmediaiconspopup.style.display = "none";
+    avatarimage.style.display = "block";
+    p_username.style.display = "block";
+    p_date.style.display = "block";
+    div_userinfo.style.backgroundColor = "white";
+    avatarcontainer.style.backgroundColor = "white";
+    avatarcontainer.style.borderRadius = "0 0 0 15px";
+    sharebutton.style.backgroundColor = "white";
+    sharebutton.style.borderRadius = "0 0 15px 0";
+    sharebutton.style.color = "black";
+    svg_sharebutton.style.backgroundColor = "#ecf2f8";
 
-            p_sharemsg.style.display = "none";
-            socialmediaicons.style.display = "none";
-            svg_sharebutton.style.color = "currentColor";
-            svg_sharebutton.style.backgroundColor = "#ecf2f8";
+    share_msg.style.display = "none";
+    socialmediaicons.style.display = "none";
 
-        // set share button active
-        } else {
-            userinfoshare.classList.add("user-info-share-active");
+}
 
-            avatarimage.style.display = "none";
-            p_username.style.display = "none";
-            p_date.style.display = "none";
+function SelectToggleFunction() {
+    if(window.innerWidth < 870) { 
+        ToggleActiveMobile();
+     } else {
+         ToggleActiveDesktop();
+     }
 
-            p_sharemsg.style.display = "block";
-            socialmediaicons.style.display = "flex";
-            svg_sharebutton.style.color = "white";
-            svg_sharebutton.style.backgroundColor = "#6d7f97";
+}
 
-        }
+function ToggleActiveMobile() { 
+    if(!(avatarimage.classList.contains("hide"))) {
+        avatarimage.classList.add("hide");
+        avatarimage.style.display = "none";
+        p_username.style.display = "none";
+        p_date.style.display = "none";
+
+        div_userinfo.style.backgroundColor = "#48556a";
+        avatarcontainer.style.backgroundColor = "#48556a";
+        avatarcontainer.style.borderRadius = "0 0 0 15px";
+        sharebutton.style.backgroundColor = "#48556a";
+        sharebutton.style.borderRadius = "0 0 15px 0";
+        sharebutton.style.color = "white";
+        svg_sharebutton.style.backgroundColor = "#6d7f97";
+
+        share_msg.style.display = "block";
+        socialmediaicons.style.display = "block";
     } else {
-        if(socialmediapopup.classList.contains("social-media-icons-popup-active")) {
-            socialmediapopup.classList.remove("social-media-icons-popup-active");
-            svg_sharebutton.style.color = "currentColor";
-            svg_sharebutton.style.backgroundColor = "#ecf2f8";
+        avatarimage.classList.remove("hide");
+        avatarimage.style.display = "block";
+        p_username.style.display = "block";
+        p_date.style.display = "block";
+        
+        div_userinfo.style.backgroundColor = "white";
+        avatarcontainer.style.backgroundColor = "white";
+        avatarcontainer.style.borderRadius = "0 0 0 15px";
+        sharebutton.style.backgroundColor = "white";
+        sharebutton.style.borderRadius = "0 0 15px 0";
+        sharebutton.style.color = "black";
+        svg_sharebutton.style.backgroundColor = "#ecf2f8";
 
-        } else {
-            socialmediapopup.classList.add("social-media-icons-popup-active");
-            socialmediapopup.style.backgroundColor = "#6d7f97";
-            svg_sharebutton.style.color = "white";
-            svg_sharebutton.style.backgroundColor = "#6d7f97";
-        }
+        share_msg.style.display = "none";
+        socialmediaicons.style.display = "none";
     }
-    
 
+}
+
+function ToggleActiveDesktop() {
+    if(!(socialmediaiconspopup.classList.contains("hide"))) {
+        socialmediaiconspopup.classList.add("hide");
+        socialmediaiconspopup.style.display = "none";
+
+        svg_sharebutton.style.backgroundColor = "#ecf2f8";
+        sharebutton.style.borderRadius = "0 0 15px 0";
+        sharebutton.style.color = "black";
+    } else {
+        socialmediaiconspopup.classList.remove("hide");
+        socialmediaiconspopup.style.display = "block";
+
+        sharebutton.style.backgroundColor = "white";
+        sharebutton.style.borderRadius = "0 0 15px 0";
+        sharebutton.style.color = "white";
+
+        svg_sharebutton.style.backgroundColor = "#48556a";
+    }
 }
